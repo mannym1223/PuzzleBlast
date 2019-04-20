e0bf289836b49ddd1591467a4d387c24541631d7
Manny <mmunoz38@miners.utep.edu>
2019-04-19 20:43:34 -0600

    Game grid for keeping track of squares

--- src/main/java/edu/utep/cs/cs4330/puzzleblast/GameGrid.java
+++ src/main/java/edu/utep/cs/cs4330/puzzleblast/GameGrid.java
@@ -2,26 +2,34 @@ package edu.utep.cs.cs4330.puzzleblast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class GameGrid {
 
-    private static List<List<Square>> squares;
+    private static List<Square> squares;
     private static int gridSize;
+    private static final GameGrid INSTANCE = new GameGrid();
 
-    public GameGrid() {
+    private GameGrid() {
         if(squares == null) {
             squares = initSquares();
             gridSize = 4;
         }
     }
+    public static GameGrid getInstance() {
+        return INSTANCE;
+    }
 
-    private List<List<Square>> initSquares() {
-        List<List<Square>> newList = new ArrayList<>();
+
+
+    private List<Square> initSquares() {
+        Random rand = new Random();
+        List<Square> newList = new ArrayList<>();
 
         for(int row = 0; row < gridSize;row++) {
             for(int col = 0; col < gridSize;col++) {
-                Square square = new Square(row, col, randomValue());
-                newList.get(row).add(col, square);
+                Square square = new Square(row, col, rand.nextInt(1) *2);
+                newList.add(square);
             }
         }
 
