00b64d74be20ff6547bbe1794a216674f76fd1f2
Manny <mmunoz38@miners.utep.edu>
2019-04-20 14:47:26 -0600

    square now contains drawable

--- src/main/java/edu/utep/cs/cs4330/puzzleblast/Square.java
+++ src/main/java/edu/utep/cs/cs4330/puzzleblast/Square.java
@@ -1,18 +1,23 @@
 package edu.utep.cs.cs4330.puzzleblast;
 
+import android.graphics.drawable.Drawable;
+
 public class Square {
 
     private int row;
     private int col;
     private int value;
+    private Drawable image;
 
-    public Square(int startRow, int startCol, int startValue) {
+    public Square(int startRow, int startCol, int startValue, Drawable img) {
         value=startValue;
         row=startRow;
         col=startCol;
-
+        image = img;
     }
 
-
+    public Drawable getImage() {
+        return image;
+    }
 
 }
