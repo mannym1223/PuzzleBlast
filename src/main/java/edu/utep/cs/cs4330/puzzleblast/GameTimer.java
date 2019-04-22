5c5379fde1e546014d3df85f26b9a04349c9fec4
Darayner2177 <49380308+Darayner2177@users.noreply.github.com>
2019-04-22 15:14:15 -0600

    Add files via upload

---
+++ src/main/java/edu/utep/cs/cs4330/puzzleblast/GameTimer.java
@@ -0,0 +1,66 @@
+package cs4330.cs.utep.puzzletest;
+
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class GameTimer {
+
+    private CountDownTimer countDownTimer;
+    private long timeRemaining;
+    private long startTime;
+    private boolean isRunning;
+    private static final long GAME_TIME_INTERVAL = 1000;
+
+    public GameTimer(long startTime) {
+        this.startTime = startTime;
+        this.timeRemaining = startTime;
+    }
+
+
+    public void startGameTimer(){
+        countDownTimer = new CountDownTimer(timeRemaining, GAME_TIME_INTERVAL) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeRemaining = millisUntilFinished;
+            }
+
+            @Override
+            public void onFinish() {
+                isRunning = false;
+            }
+        }.start();
+
+        isRunning = true;
+
+    }
+
+
+    public void updateGameTimerText(){
+        int minutes = (int) (timeRemaining / 1000) / 60;
+        int seconds = (int) (timeRemaining / 1000) % 60;
+
+        String timerText = String.format(Locale.getDefault(), "%02d:02d", minutes, seconds);
+
+    }
+
+    private void pauseTimer(){
+        countDownTimer.cancel();
+        isRunning = false;
+    }
+
+    private void resetTimer(){
+        timeRemaining = startTime;
+        updateGameTimerText();
+
+    }
+
+    private void setTimer(long timer){
+
+
+    }
+
+
+}
