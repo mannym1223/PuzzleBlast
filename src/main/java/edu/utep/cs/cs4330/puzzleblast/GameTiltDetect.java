dbd837022a15d604fda3a95fbcd116267fef6775
Manny <mmunoz38@miners.utep.edu>
2019-04-26 18:15:52 -0600

    fixed motion sensor updating too frequently

--- src/main/java/edu/utep/cs/cs4330/puzzleblast/GameTiltDetect.java
+++ src/main/java/edu/utep/cs/cs4330/puzzleblast/GameTiltDetect.java
@@ -15,15 +15,19 @@ public class GameTiltDetect implements SensorEventListener {
     private Sensor magnetometer;
     private float[] accelerometerValues;
     private float[] magnometerValues;
-    private float[] gyroValues;
     private float azimuth;
     private float pitch;
     private float roll;
+    private float azimuth_last;
+    private float roll_last;
+    private float pitch_last;
+    private GameGrid grid;
 
     public GameTiltDetect(Context context){
-        sensorManager = (SensorManager) context.getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        grid = GameGrid.getInstance();
 
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager = (SensorManager) context.getApplicationContext().getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
         initListeners();
@@ -32,9 +36,8 @@ public class GameTiltDetect implements SensorEventListener {
 
 
     public void initListeners() {
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
-        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
-        //sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -43,68 +46,81 @@ public class GameTiltDetect implements SensorEventListener {
         float[] rotationMatrix = new float[9];
         float orientationValues[] = new float[3];
         boolean rotation = false;
+        long curTime = System.currentTimeMillis();
+        long lastUpdate = 0;
 
         switch (sensorType) {
-            case Sensor.TYPE_ACCELEROMETER:
+            case Sensor.TYPE_GRAVITY:
                 accelerometerValues = event.values.clone();
                 break;
             case Sensor.TYPE_MAGNETIC_FIELD:
                 magnometerValues = event.values.clone();
                 break;
-            case Sensor.TYPE_GYROSCOPE:
-                gyroValues = event.values.clone();
-                break;
             default:
                 return;
         }
+
         if(accelerometerValues != null && magnometerValues != null){
             rotation = SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerValues, magnometerValues);
         }
 
         if (rotation) {
             SensorManager.getOrientation(rotationMatrix, orientationValues);
-            azimuth = orientationValues[0];
-            pitch = orientationValues[1];
-            roll = orientationValues[2];
+            if ((curTime - lastUpdate) > 100) {
+                lastUpdate = curTime;
 
-            determineTilt();
+                azimuth = orientationValues[0];
+                pitch = orientationValues[1];
+                roll = orientationValues[2];
+
+                determineTilt(roll, roll_last, pitch, pitch_last);
+
+                azimuth_last = azimuth;
+                pitch_last = pitch;
+                roll_last = roll;
+
+            }
         }
 
     }
 
-    public void pause(){
-        sensorManager.unregisterListener(this);
-    }
-
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 
-    public void determineTilt() {
-            float degrRoll = (float) (Math.toDegrees(roll));
-            float degrPitch = (float) (Math.toDegrees(pitch));
+    public void determineTilt(float roll, float roll_last, float pitch, float pitch_last) {
+        float degrRoll = (float) (Math.toDegrees(roll));
+        float degrPitch = (float) (Math.toDegrees(pitch));
+        float degrRoll_last = (float) (Math.toDegrees(roll_last));
+        float degrPitch_last = (float) (Math.toDegrees(pitch_last));
 
-            if (degrRoll < -45) {
-                onLeftTilt();
-                Log.d("Tilt", "LeftTilt");
-            }
-            else if (degrRoll > 45) {
-                onRightTilt();
-                Log.d("Tilt", "RightTilt");
-            }
-            else if (degrPitch > 25) {
-                onUpTilt();
-                Log.d("Tilt", "UpTilt");
-            }
-            else if (degrPitch < -25) {
-                onDownTilt();
-                Log.d("Tilt", "DownTilt");
-            }
-            else {
-                onCentered();
-                Log.d("Tilt", "Center");
-            }
+        if((degrRoll > 90 || degrRoll < -90)){
+            Log.d("Tilt", "Faulty Reading:");
+        }
+        else if ((degrPitch > 30) && Math.abs(degrPitch - degrPitch_last) > 30){
+            Log.d("Tilt", "UpTilt: " + degrPitch);
+            onUpTilt();
+        }
+        else if ((degrPitch < -30) && Math.abs(degrPitch - degrPitch_last) > 30){
+            Log.d("Tilt", "DowntTilt: " + degrPitch);
+            onDownTilt();
+        }
+
+        else if((degrRoll < -30) && Math.abs(degrRoll - degrRoll_last) > 30){
+            Log.d("Tilt", "LeftTilt: " + degrRoll);
+            onLeftTilt();
+        }
+
+        else if((degrRoll > 30) && Math.abs(degrRoll - degrRoll_last) > 30){
+            Log.d("Tilt", "RightTilt: " + degrRoll);
+            onRightTilt();
+        }
+
+        else{
+            Log.d("Tilt", "Center");
+            onCentered();
+        }
+
     }
 
     public void onLeftTilt() {
