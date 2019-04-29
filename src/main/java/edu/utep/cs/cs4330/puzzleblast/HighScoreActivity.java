f11bde8564e20220a32967768e459b6449fe5171
Manny <mmunoz38@miners.utep.edu>
2019-04-29 11:22:57 -0600

    database files

---
+++ src/main/java/edu/utep/cs/cs4330/puzzleblast/HighScoreActivity.java
@@ -0,0 +1,34 @@
+package edu.utep.cs.cs4330.puzzleblast;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+public class HighScoreActivity extends AppCompatActivity {
+
+    GameScoreCursorAdapter gameScoreCursorAdapter;
+    ListView listView;
+    Cursor cursor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_high_score);
+
+        listView = findViewById(R.id.gameScoreListView);
+        GameDBHelper gameDBHelper = new GameDBHelper(this);
+
+        cursor = gameDBHelper.allScores();
+
+
+        new Handler().post(() -> {
+            gameScoreCursorAdapter = new GameScoreCursorAdapter(HighScoreActivity.this, cursor);
+            listView.setAdapter(gameScoreCursorAdapter);
+        });
+
+
+
+    }
+}
