c90449dd8553904215e77195eb13502100ff55ca
Manny <mmunoz38@miners.utep.edu>
2019-04-29 13:52:37 -0600

    timer updates given textView

---
+++ src/main/java/edu/utep/cs/cs4330/puzzleblast/GameTimer.java
@@ -0,0 +1,76 @@
+package edu.utep.cs.cs4330.puzzleblast;
+
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
+public class GameTimer {
+
+    private CountDownTimer countDownTimer;
+    private long timeRemaining;
+    private long startTime;
+    private boolean isRunning;
+    private TextView timerText;
+    private static final long GAME_TIME_INTERVAL = 1000;
+
+    public GameTimer(long startTime, TextView text) {
+        this.startTime = startTime;
+        this.timeRemaining = startTime;
+        timerText = text;
+    }
+
+
+    public void startGameTimer(){
+        countDownTimer = new CountDownTimer(timeRemaining, GAME_TIME_INTERVAL) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeRemaining = millisUntilFinished;
+                updateGameTimerText();
+            }
+
+            @Override
+            public void onFinish() {
+                isRunning = false;
+            }
+        }.start();
+
+        isRunning = true;
+
+    }
+
+
+    public void updateGameTimerText(){
+        int minutes = (int) (timeRemaining / 1000) / 60;
+        int seconds = (int) (timeRemaining / 1000) % 60;
+
+        timerText.setText(String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds));
+
+    }
+
+    public void pauseTimer(){
+        countDownTimer.cancel();
+        isRunning = false;
+    }
+
+    public void resumeTimer() {
+        countDownTimer.start();
+        isRunning = true;
+    }
+
+    public void resetTimer(){
+        timeRemaining = startTime;
+        updateGameTimerText();
+
+    }
+
+    public void setTimer(long timer){
+    }
+
+    public long getTimeRemaining() {
+        return timeRemaining;
+    }
+}
